5c5c2dbb6f289a74ff4019ba0826edcaa61e36cf
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-01 21:34:05 +0100

    started weatherapp, basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import WeatherApp from "./WeatherApp/Weatherapp"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App</h1>
-        <div className="row"></div>
-        <div> WeatherSearch</div>
-        <div>
-          <Container fluid>
-            <Row>
-              <Col>1 of 1</Col>
-            </Row>
-          </Container>
-        </div>
+        <WeatherApp/>
       </div>
     </div>
   );
